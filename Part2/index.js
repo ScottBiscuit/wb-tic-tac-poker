100f821666dea59dda000358a00e4146fc7581aa
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-13 16:39:07 -0500

    completed

--- Part2/index.js
+++ Part2/index.js
@@ -2,5 +2,11 @@ const idInput = document.querySelector('#idInput');
 
 const colorInput = document.querySelector('#colorInput');
 
-console.log(idInput)
-console.log(colorInput)
+
+function setCard(){
+    const card = document.querySelector(`#${idInput.value}`);
+    console.log(card);
+    card.style.color = colorInput.value;
+}
+
+document.querySelector("#editCardButton").addEventListener('click', setCard)
